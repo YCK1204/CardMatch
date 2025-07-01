e4d7f7f3c526e15f1499cea6a14021ccc1d2f0ad
KimJunHyeong <kheami35@gmail.com>
2025-07-01 10:08:50 +0900

    Add files via upload
    card board 스크립트 및 main에서 start 버튼을 눌렀을때 그 스테이지의 index를 이용해  main에서 카드 배치

---
+++ CardMatch/Assets/Scripts/Board.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Board : MonoBehaviour
+{
+    public GameObject card;
+
+    void Start()
+    {
+        switch (Level.selectLevelindex) // Main Scene에서 Stage를 누른 값을 기반으로 카드 생성
+        {
+            case 1:
+                int[] easy = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
+                SpawnCard(easy);
+                break;
+
+            case 2:
+                int[] normal = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 0, 0, 1, 1 }; //카드 나오는지 테스트 배열 추후 8번과 9번으로 바꿔야함
+                SpawnCard(normal);
+                break;
+
+            case 3:
+                int[] hard = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 0, 0, 1, 1, 2, 2, 3, 3 }; // 위와동일
+                SpawnCard(hard);
+                break;
+
+        }
+
+    }
+
+    void SpawnCard(int[] originalArr)
+    {
+        int[] arr = originalArr.OrderBy(x => Random.Range(0f, 7f)).ToArray();
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            GameObject go = Instantiate(card, this.transform);
+
+            float x = (i % 4) * 1.4f - 2.1f;
+            float y = (i / 4) * 1.4f - 4.0f;
+
+            go.transform.position = new Vector2(x, y);
+            go.GetComponent<Card>().Setting(arr[i]);
+        }
+
+        GameManager.instance.cardCount = arr.Length;
+    }
+}
---
+++ CardMatch/Assets/Scripts/Card.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public int idx = 0;
+
+    public GameObject front;
+    public GameObject back;
+    public Animator anim;
+    public SpriteRenderer frontImage;
+    public void Setting(int number)
+    {
+        idx = number;
+        //frontImage.sprite = Resources.Load<Sprite>($"rtan{idx}"); //sprite 이름 통일
+    }
+
+    public void OpenCard()
+    {
+        anim.SetBool("IsOpen", true);
+        front.SetActive(true);
+        back.SetActive(false);
+
+        /*if (GameManager.instance.firstCard == null)  // 현재 GameManager이 없어서 오류가 떠 주석
+        {
+            GameManager.instance.firstCard = this;
+        }
+        else
+        {
+            GameManager.instance.secondCard = this;
+            GameManager.instance.Matched();
+        }*/
+    }
+    public void DestroyCard()
+    {
+        Invoke("DestoryCardInvoke", 0.5f);
+    }
+    void DestoryCardInvoke()
+    {
+        Destroy(gameObject);
+    }
+
+    public void CloseCard()
+    {
+        Invoke("CloseCardInvoke", 0.5f);
+    }
+
+    void CloseCardInvoke()
+    {
+        anim.SetBool("IsOpen", false);
+        front.SetActive(false);
+        back.SetActive(true);
+    }
+}
